e277afc1b3c12f856209a37966528463e68cdde3
segalau <au.segal@gmail.com>
2018-09-28 12:02:45 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <limits>
+#include <fstream>
+#include <cstdlib>
+#include <random>
+#include <ctime>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
